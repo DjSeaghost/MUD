dae0d9d6559b3ea0a2f9b937fd6647a3ecf7e0d6
Ryan Thames <ryanthames@gmail.com>
2013-01-25 00:56:02 -0600

    Started making rooms

--- MUD/src/mud/MUD.java
+++ MUD/src/mud/MUD.java
@@ -1,19 +1,44 @@
-/**
- * 
- */
 package mud;
+
+import java.util.Scanner;
 
 /**
  * @author ryan
- *
+ * 
  */
-public class MUD {
-
+public class MUD
+{
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		System.out.println("TESTING!!");
-		System.out.println("DUDE BRO!!");
+	public static void main(String[] args)
+	{
+		System.out.println("A Dark Corridor");
+		System.out
+				.println("You awaken to find yourself in a dark corridor. The only visible light is emanating from a torch \n"
+						+ "far off in the distance to the north. The floor is made of cold, wet stone, and is covered in \n"
+						+ "cobwebs.");
+		System.out.println("Exits: North");
+
+		while (true)
+		{
+			Scanner scanner = new Scanner(System.in);
+			String command = scanner.nextLine();
+
+			if (command.equalsIgnoreCase("exit"))
+			{
+				System.out.println("Leaving the fantasy world...");
+				System.exit(1);
+			}
+			else if (command.equalsIgnoreCase("n")
+					|| command.equalsIgnoreCase("north"))
+			{
+				System.out.println("You fall over because you're drunk bitch");
+			}
+			else
+			{
+				System.out.println("Command " + command + " not found");
+			}
+		}
 	}
 }
